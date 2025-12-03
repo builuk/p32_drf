ee0402f8bc3441ec6f165fdbd9c0d1deb185edc4
aabuiluk <andrii.a.builuk@gmail.com>
2025-12-03 18:01:32 +0200

    admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "created_at")
