c17b45158d302cf6f6e8646602bb2778a89b7257
aabuiluk <andrii.a.builuk@gmail.com>
2025-12-03 19:22:55 +0200

    Task 1

--- posts/urls.py
+++ posts/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     re_path(r'^posts/(?P<pk>\d+)/?$', views.post_detail, name='post_detail'),
     path('comments/', views.comment_list, name='post_list'),
     re_path(r'^comments/(?P<pk>\d+)/?$', views.comment_detail, name='post_detail'),
+    path('time-info/', views.time_info, name='time-info'),
+    # path('greeting/', views.greeting, name='greeting'),
+    # path('math/', views.math_operations, name='math-operations'),
+    # path('power/', views.power_operations, name='power-operations'),
 ]
