abe3aad933200e4580df4b23e0b21052ce2ebf4b
aabuiluk <andrii.a.builuk@gmail.com>
2025-12-03 18:50:52 +0200

    post_detail

--- posts/models.py
+++ posts/models.py
@@ -7,3 +7,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
