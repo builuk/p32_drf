69cf593417288459229a9a23db1a635c02c84917
aabuiluk <andrii.a.builuk@gmail.com>
2025-12-03 18:42:21 +0200

    post_detail

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
-    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
+    re_path(r'^posts/(?P<pk>\d+)/?$', views.post_detail, name='post_detail'),
 ]
